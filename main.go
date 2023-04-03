4d57b717bae56313c2cbd8c5433e8d1e3795b69c
Akhyar Amarullah <akhy@chickenzord.com>
2023-04-03 10:24:38 +0700

    Add golangci-lint job

--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func main() {
 
 	log.Level(zerolog.Level(zerolog.DebugLevel))
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Info().Int("count", len(cfg.KSEI.Accounts)).Msg("KSEI accounts loaded")
 
+	log.Info().Int("count", len(cfg.KSEI.Accounts)).Msg("KSEI accounts loaded")
 	log.Info().Msg("initializing metrics")
 	exp, err := exporter.New(cfg.KSEI)
 	if err != nil {
@@ -32,6 +32,7 @@ func main() {
 	}
 
 	log.Info().Msg("starting background metrics updater")
+
 	go func() {
 		exp.WatchMetrics()
 	}()
@@ -43,6 +44,7 @@ func main() {
 	r.Get("/metrics", exp.HTTPHandler().ServeHTTP)
 
 	log.Info().Msgf("server listening on %s", cfg.Server.BindAddress())
+
 	if err := http.ListenAndServe(cfg.Server.BindAddress(), r); err != nil {
 		panic(err)
 	}
